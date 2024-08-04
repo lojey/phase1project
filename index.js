1ba742559805ca0309a8d3f4e0123df7d758e7e3
Gloria Jeyech <gjeyech@gmail.com>
2024-08-04 09:06:11 +0300

    js file

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function showServiceDetails(serviceId) {
     const services = {
-        SameDay: 'Our same-day delivery service ensures your packages are delivered within the same day,with zerro delays ',
-        NextDay: 'With our next-day delivery service, your packages will arrive the following day.',
-        International: 'With our fast,affordable and secure courrier services we do provide shipping to various countries around the world',
-        Bulk: 'Our bulk shipment service handles large volumes of goods efficiently, securely and with zero damages whatsoever.'
+        SameDay: 'At Carryco  we understand the importance of speed and reliability when it comes to delivering your packages. That is why we offer a premium same-day delivery service designed to meet your urgent delivery needs.Our same-day delivery service ensures your packages are delivered within the same day,with zerro delays.',
+        NextDay: 'Our next-day delivery service is designed to provide you with fast, reliable, and efficient delivery solutions. Whether you need to send important documents, parcels, or packages, you can trust us to get them to their destination by the next day.',
+        International: ' our global network ensures that your items reach their destination safely and on time. We offer a range of shipping options to suit your needs, from express delivery for urgent shipments to economy options for cost-effective solutions.',
+        Bulk: 'At Caaryco Courier Company, we understand the complexities and demands of bulk shipping. Our dedicated bulk shipping service is designed to cater to businesses and individuals who need to move large volumes of goods efficiently and cost-effectively.'
     };
 
     const serviceDetailSection = document.getElementById('serviceDetails');
@@ -77,3 +77,29 @@ function enableNotifications() {
     alert('Notifications enabled!');
 }
 
+document.addEventListener('DOMContentLoaded', async function() {
+    const apiDataHolder = document.getElementById('apiContent');
+
+    try {
+        const response = await axios.get('http://localhost:3000/profiles');
+        const posts = response.data;
+
+        console.log(posts);
+
+        posts.forEach(post => {
+            const postElement = document.createElement('div');
+            postElement.classList.add('post-card');
+
+            postElement.innerHTML = 
+                `<img src="${post.image}" alt="profile image"/>
+                <h3>NAME: ${post.name}</h3>
+                <p>AGE: ${post.age}</p>
+                <p>RATINGS: ${post.rating}</p>
+
+            `;
+            apiDataHolder.appendChild(postElement);
+        });
+    } catch (error) {
+        apiDataHolder.innerHTML = '<p>FAILED TO FETCH DATA..</p>';
+    }
+});
